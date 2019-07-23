50a0105407af8fa30e61e7afdcb3043e5b61f98d
ByteKP <kamolpc@windowlive.com>
2019-07-23 14:40:25 +0700

    successfully create new project

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import errorReducers from "./errorReducers";
 
-export default combineReducers({});
+export default combineReducers({
+  errors: errorReducers
+});
