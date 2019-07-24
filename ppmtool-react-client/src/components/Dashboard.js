9290e7f3bdf516dc7233291ea9cb0fefe90ecb7c
ByteKP <kamolpc@windowlive.com>
2019-07-24 10:46:42 +0700

    dynamically display projectitems

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -6,10 +6,16 @@ import { getProjects } from "../actions/projectActions";
 import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  //2nd. The comp can now uses getProjects dispatch method
+  //to in turn dispatch GET_PROJECTS with payload to the projectReducers
+  //the projectReducers then destructure the payload to its state
   componentDidMount() {
     this.props.getProjects();
   }
   render() {
+    //4th. the comp can now use completed reducer's state
+    //through destructuring in its render()
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -20,7 +26,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -32,10 +40,14 @@ Dashboard.propTypes = {
   project: PropTypes.object.isRequired
 };
 
+//3rd. Map projectReducers' new state after destructure to
+//the comp props
 const mapStateToProps = state => ({
   project: state.project
 });
 
+//1st step. The comp connects to redux store and
+//map both state(initial) and dispatch method(getProjects) to the comp's props
 export default connect(
   mapStateToProps,
   { getProjects }
