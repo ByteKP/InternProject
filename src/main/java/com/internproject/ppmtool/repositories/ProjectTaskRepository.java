2b87aca530c9b8f49af041e23da5ddf85fb6830d
ByteKP <kamolpc@windowlive.com>
2019-07-24 15:30:51 +0700

    added Backlog and ProjectTask entities

---
+++ src/main/java/com/internproject/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.internproject.ppmtool.repositories;
+
+import com.internproject.ppmtool.domain.ProjectTask;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
+}
