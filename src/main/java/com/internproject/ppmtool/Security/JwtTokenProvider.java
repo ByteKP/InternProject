d051862cccae89c52dc926fb72e755ff00519c37
ByteKP <kamolpc@windowlive.com>
2019-07-31 13:36:20 +0700

    Finished generated Jwt token in header after success login/ Authorization complete

--- src/main/java/com/internproject/ppmtool/Security/JwtTokenProvider.java
+++ src/main/java/com/internproject/ppmtool/Security/JwtTokenProvider.java
@@ -1,5 +1,8 @@
 package com.internproject.ppmtool.Security;
 
+import static com.internproject.ppmtool.Security.SecurityConstraints.EXPIRATION_TIME;
+import static com.internproject.ppmtool.Security.SecurityConstraints.SECRET;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,11 +12,13 @@ import com.internproject.ppmtool.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-
-import static com.internproject.ppmtool.Security.SecurityConstraints.EXPIRATION_TIME;
-import static com.internproject.ppmtool.Security.SecurityConstraints.SECRET;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class JwtTokenProvider {
@@ -32,5 +37,29 @@ public class JwtTokenProvider {
 
         return Jwts.builder().setSubject(userId).setClaims(claims).setIssuedAt(now).setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, SECRET).compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT ");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims String empty");
+        }
+        return false;
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
     }
 }
--- src/main/java/com/internproject/ppmtool/Security/SecurityConfig.java
+++ src/main/java/com/internproject/ppmtool/Security/SecurityConfig.java
@@ -16,7 +16,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
@@ -42,6 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css", "/**/*.js")
                 .permitAll().antMatchers(SIGN_UP_URLS).permitAll().antMatchers(H2_URL).permitAll().anyRequest()
                 .authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
