8004e157a28c58b5e46b6259cc766656bca74837
ByteKP <kamolpc@windowlive.com>
2019-08-02 14:25:16 +0700

    controlled login form

--- ppmtool-react-client/src/securityUtils/setJWTToken.js
+++ ppmtool-react-client/src/securityUtils/setJWTToken.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const setJWTToken = token => {
   if (token) {
-    axios.setdefaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common["Authorization"] = token;
   } else {
-    delete axios.setdefaults.headers.common["Authorization"];
+    delete axios.defaults.headers.common["Authorization"];
   }
 };
 
